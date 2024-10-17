6ec679f860e833ad36588276935733fecbcd908d
Christopher GERARD <christopher.gerard@ikmail.com>
2024-10-17 22:02:08 +0200

    CHG : Les vérifications des champs d'email, de numéro de téléphone et de mots de passe se font désormais sur la saisie utilisateur en direct
    Signed-off-by: Christopher GERARD <christopher.gerard@ikmail.com>

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,9 +11,18 @@ import { empty } from 'rxjs';
 })
 
 export class FormComponent {
-  valid: Boolean = true;
-  hidden: Boolean = true;
-  formIsValid: Boolean = false;
+  disableSubmitButton: Boolean = true;
+  hideTable: Boolean = true;
+  hideForm: Boolean = false;
+  hidePasswordMismatchMessage: Boolean = true;
+  hidePasswordMatchMessage: Boolean = true;
+  hidePasswordLengthAlertMessage: Boolean = true;
+  hideInvalidEmailMessage: Boolean = true;
+  hideInvalidPhoneMessage: Boolean = true;
+
+  emailIsValid: Boolean = false;
+  passwordIsValid: Boolean = false;
+  phoneIsValid: Boolean = true;
 
   civility: string = "";
   firstName: string = "";
@@ -33,27 +42,79 @@ export class FormComponent {
       alert("Tous les champs obligatoires (*) ne sont pas renseignés");
     }
     else {
-      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-      if (!emailRegex.test(this.email)) {
-        alert("Email invalide.");
+      if (this.emailIsValid && this.passwordIsValid && this.phoneIsValid) {
+        this.disableSubmitButton = !this.disableSubmitButton;
+        this.hideTable = !this.hideTable;
+        this.hideForm = !this.hideForm;
       }
-      else
-      {
-        if (this.password != this.passwordValidation) {
-          alert("Les mots de passe ne sont pas égaux");
+      else {
+        alert("Certains champs sont invalides");
+      }
+    }
+  }
+
+  onPasswordInput() {
+    if (this.password == "" && this.passwordValidation == "") {
+      this.hidePasswordMismatchMessage = true;
+      this.hidePasswordMatchMessage = true;
+      this.passwordIsValid = false;
+    }
+    else {
+      if (this.passwordValidation != this.password) {
+        this.hidePasswordMismatchMessage = false;
+        this.hidePasswordMatchMessage = true;
+        this.passwordIsValid = false;
+      }
+      else {
+        if (this.password.length < 8) {
+          this.hidePasswordMismatchMessage = true;
+          this.hidePasswordMatchMessage = false;
+          this.hidePasswordLengthAlertMessage = false;
+          this.passwordIsValid = false;
         }
-        else
-        {
-          if (this.password.length < 8)
-          {
-            alert("Le mot de passe doit contenir au moins 8 caractères");
-          }
-          else
-          {
-            this.valid = !this.valid
-            this.hidden = !this.hidden
-          }
+        else {
+          this.hidePasswordMismatchMessage = true;
+          this.hidePasswordMatchMessage = false;
+          this.hidePasswordLengthAlertMessage = true;
+          this.passwordIsValid = true;
         }
+      }
+    }
+  }
+
+  onEmailInput() {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (this.email == "") {
+      this.hideInvalidEmailMessage = true;
+      this.emailIsValid = false;
+    }
+    else {
+      if (!emailRegex.test(this.email)) {
+        this.hideInvalidEmailMessage = false;
+        this.emailIsValid = false;
+      }
+      else {
+        this.hideInvalidEmailMessage = true;
+        this.emailIsValid = true;
+      }
+    }
+  }
+
+  onPhoneInput() {
+    const phoneRegex = /^(\+\d{1,3}|0)[1-9](\s?\d{2}){4}$/;
+
+    if (this.phone?.toString().length == 0) {
+      this.hideInvalidPhoneMessage = true;
+      this.phoneIsValid = true;
+    }
+    else {
+      if (this.phone != null && phoneRegex.test(this.phone.toString())) {
+        this.hideInvalidPhoneMessage = true;
+        this.phoneIsValid = true;
+      }
+      else {
+        this.hideInvalidPhoneMessage = false;
+        this.phoneIsValid = false;
       }
     }
   }
